e3eae9028fef749860d7853a6da486c2c2f4a496
CptJackFrost <cptjackfrost74@gmail.com>
2020-07-12 23:49:31 +0500

    Тесты проходятся

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -6,25 +6,35 @@ import org.testng.annotations.Test;
 
 public class CalculatorTest {
 
-
-
     @DataProvider
-    public Object[][] testObjects(){
+    public Object[][] testObjectsPositive(){
         return new Object[][] {
                 {2, 3, '+', 5},
-                {2, 2, '+', 10},
                 {3, 1, '-', 2},
-                {100, 200, '-', 12},
                 {5, 2, '/', 2},
-                {6, 3, '/', 1},
                 {4, 3, '*', 12},
-                {"one", 2, '+', 3}
+
         };
     }
 
-    @Test(dataProvider = "testObjects")
-    public void calcTest(int first, int second, char operation, int result){
+    @Test(dataProvider = "testObjectsPositive")
+    public void calcTestPositive(int first, int second, char operation, int result){
         Assert.assertEquals(result, Calculator.calc(first, second, operation), "Значения не равны");
+    }
+
+    @DataProvider
+    public Object[][] testObjectsWrong(){
+        return new Object[][] {
+                {2, 2, '+', 10},
+                {100, 200, '-', 12},
+                {6, 3, '/', 1},
+                {5, 5, '*', 100}
+        };
+    }
+
+    @Test(dataProvider = "testObjectsWrong")
+    public void calcTestWrongResult(int first, int second, char operation, int result){
+        Assert.assertNotEquals(result, Calculator.calc(first, second, operation), "Значения равны");
     }
 
 }
